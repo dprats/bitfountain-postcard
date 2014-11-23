7725cc18d410a908f6cfd12feaba9fb1a4b6d2a5
Diego Prats <diego.prats@gmail.com>
2014-11-23 10:48:06 -0800

    Initial Commit

---
+++ bitfountain-postcard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bitfountain-postcard
+//
+//  Created by diego prats on 11/23/14.
+//  Copyright (c) 2014 diego prats. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
